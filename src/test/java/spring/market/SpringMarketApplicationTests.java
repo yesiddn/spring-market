26c9b8c9efab6c375d6ac7439a62b77bdc9cb115
Yesid <yesidrodriguez305@gmail.com>
2024-02-27 07:45:41 -0500

    Add init structure

---
+++ src/test/java/spring/market/SpringMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMarketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
