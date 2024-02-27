83bf79cf2c18eae9877959f80aa61cb68d76c2c2
Yesid <yesidrodriguez305@gmail.com>
2024-02-27 08:44:04 -0500

    Add hello world and application.properties

---
+++ src/main/java/spring/market/HelloWorldController.java
@@ -0,0 +1,14 @@
+package spring.market;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greet")
+public class HelloWorldController {
+  @GetMapping("/hello")
+  public String greeting() {
+    return "Hello World!";
+  }
+}
