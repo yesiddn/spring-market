dd5c18bae53f21952f6ed012dfcceab1d883e84d
Yesid <yesidrodriguez305@gmail.com>
2024-02-29 11:05:40 -0500

    Add query methods

--- src/main/java/spring/market/persistence/ProductoRepository.java
+++ src/main/java/spring/market/persistence/ProductoRepository.java
@@ -4,11 +4,20 @@ import spring.market.persistence.crud.ProductoCrudRepository;
 import spring.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
   private ProductoCrudRepository productoCrudRepository;
 
   public List<Producto> getAll() {
     return (List<Producto>) productoCrudRepository.findAll(); // findAll retorna un Itelable, por lo cual se castea a una Lista
+  }
+
+  public List<Producto> getByCategoria(int idCategoria) {
+    return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+  }
+
+  public Optional<List<Producto>> getEscasos(int cantidad) {
+    return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
   }
 }
--- src/main/java/spring/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/spring/market/persistence/crud/ProductoCrudRepository.java
@@ -3,9 +3,23 @@ package spring.market.persistence.crud;
 import org.springframework.data.repository.CrudRepository;
 import spring.market.persistence.entity.Producto;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> { // <Table, TipoDatoPK>
 //  Tres tipos de repositorios:
 //  - CrudRepository: realizar el crue
 //  - PagingAndSortingRepository: incluye lo de Crud repository ademas de paginación y ordenamiento.
 //  - JPARepository: Ademas de tener CrudRepository y PagingAndSortingRepository nos permite tareas específicas como Flush.
+
+//  query methods, mejor practica
+//  List<Producto> findByIdCategoria(int idCategoria);
+  List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+//  forma nativa
+//  @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+//  List<Producto> getByCategoria(int idCategoria);
+
+  Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
 }
