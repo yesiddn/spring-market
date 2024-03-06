952c0abd5391fb4a5ab07d1dd81c133629186984
Yesid <yesidrodriguez305@gmail.com>
2024-03-06 08:49:55 -0500

    Add Purchase domain

---
+++ src/main/java/spring/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,13 @@
+package spring.market.domain.repository;
+
+import spring.market.domain.Product;
+import spring.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+  List<Purchase> getAll();
+  Optional<List<Purchase>> getByClientId(String clientId);
+  Purchase save(Product purchase);
+}
