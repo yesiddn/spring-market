418b75e77c9de860835eb1c9bf47f668e01f5625
Yesid <yesidrodriguez305@gmail.com>
2024-03-06 08:00:51 -0500

    Add HTTP responses

--- src/main/java/spring/market/web/controller/ProductController.java
+++ src/main/java/spring/market/web/controller/ProductController.java
@@ -1,6 +1,9 @@
 package spring.market.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import spring.market.domain.Product;
 import spring.market.domain.service.ProductService;
@@ -15,27 +18,41 @@ public class ProductController {
   private ProductService productService;
 
   @GetMapping()
-  public List<Product> getAll() {
-    return productService.getAll();
+  public ResponseEntity<List<Product>> getAll() {
+    return new ResponseEntity<>(productService.getAll(), HttpStatus.OK); // recibe dos argumentos: respuesta y http status
   }
 
   @GetMapping("/{id}")
-  public Optional<Product> getProduct(@PathVariable("id") int productId) {
-    return productService.getProduct(productId);
+  public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) { // ya no es necesario responder con un Optional
+//    return productService.getProduct(productId)
+//            .map(product -> new ResponseEntity<>(product, HttpStatus.OK)) // para ver el contenido del Optional
+//            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND)); // si no se encuentra nada en el Optional
+
+    return ResponseEntity.of(productService.getProduct(productId)); // desde spring 5 existe este método static en ResponseEntity para evitar hacer lo anterior
   }
 
   @GetMapping("/category/{id}")
-  public Optional<List<Product>> getByCategoryId(@PathVariable("id") int categoryId) {
-    return productService.getCategoryById(categoryId);
-  }
+  public ResponseEntity<List<Product>> getByCategoryId(@PathVariable("id") int categoryId) {
+    return productService.getCategoryById(categoryId)
+            .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+  } // cuando se consulta a la base de datos y se desea obtener una lista de producto por categoria y la categoria no existe entonces se devuelve una lista vacía, entonces, al hacer el map or else nunca entra al or else porque En el Optional si está presenta la lista, solo que está vacía, entonces aunque la categoria no exista siempre devolverá estatus OK -> 200.
 
   @PostMapping()
-  public Product save(@RequestBody Product product) {
-    return productService.save(product);
+  public ResponseEntity<Product> save(@RequestBody Product product) {
+    return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
   }
 
   @DeleteMapping("{id}")
-  public boolean delete(@PathVariable("id") int productId) {
-    return productService.delete(productId);
+  public ResponseEntity delete(@PathVariable("id") int productId) {
+    if (productService.delete(productId)) {
+      return new ResponseEntity<>(HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // return new ResponseEntity(this.productService.delete(productId)
+    //            ? HttpStatus.OK
+    //            : HttpStatus.NOT_FOUND);
   }
 }
